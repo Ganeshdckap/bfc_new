5fdb5ed0c474fb5421af2f95688783c2b748cfb0
Ganeshdckap <ganeshmdckap@gmail.com>
2024-07-01 23:43:24 +0530

    changes added

--- js/custom.js
+++ js/custom.js
@@ -154,4 +154,19 @@ function sendwhatsapp(){
 
 
 //   <!-- Gallery    -->
-
+var swiper = new Swiper('.swiper', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
